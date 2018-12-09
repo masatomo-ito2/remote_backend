4b30fe1607549c43733f5479499f1ab7995bbb4d
masatomo-ito2 <masa+github@hashicorp.com>
2018-12-09 20:44:52 +0900

    modified

--- remote.tf
+++ remote.tf
@@ -3,24 +3,36 @@ terraform {
 		organization = "masa_org"
 
 		workspaces {
-			prefix = "jenkins_pipeline_"
+			prefix = "Remote_proj_"
 		}
 	}
 }
 
+variable "node_count" {
+	default = "1"
+}
+
+variable "env_name" {
+	default = "QA"
+}
+
 provider "aws" {
-  region     = "ap-northeast-1"
+	region     = "ap-northeast-1"
 }
 
 resource "aws_instance" "itmstm" {
-  ami           = "ami-08847abae18baa040"
-  instance_type = "t2.micro"
+	ami				= "ami-08847abae18baa040"
+	instance_type	= "t2.micro"
+	count			= "${var.node_count}"
 
-  # My security setting
-  security_groups = ["${aws_security_group.default.name}"]
+	# My security setting
+	security_groups = ["${aws_security_group.default.name}"]
+	tags {
+		Name = "${var.env_name}_${count.index}"
+	}
 }
 
 resource "aws_security_group" "default" {
-  name        = "terraform_security_group"
-  description = "Allow access to SSH port"
+	name		= "Jenkins_security_group for ${var.env_name}"
+	description = "Allow access to SSH port"
 }
