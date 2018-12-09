8a5d31c2bdf4fd7576ec2f31b4f3be91cc185b5f
masatomo-ito2 <masa+github@hashicorp.com>
2018-12-09 20:09:44 +0900

    update

--- remote.tf
+++ remote.tf
@@ -7,3 +7,20 @@ terraform {
 		}
 	}
 }
+
+provider "aws" {
+  region     = "ap-northeast-1"
+}
+
+resource "aws_instance" "itmstm" {
+  ami           = "ami-08847abae18baa040"
+  instance_type = "t2.micro"
+
+  # My security setting
+  security_groups = ["${aws_security_group.default.name}"]
+}
+
+resource "aws_security_group" "default" {
+  name        = "terraform_security_group"
+  description = "Allow access to SSH port"
+}
